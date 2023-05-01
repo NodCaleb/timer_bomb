fb93e68de3443f2d296b057ac8aaa19a185029cf
NodCaleb <nod.caleb@gmail.com>
2023-05-01 22:16:15 +0300

    Stop interrupt - incorrect

--- timer_bomb_firmware/timer_bomb_firmware.ino
+++ timer_bomb_firmware/timer_bomb_firmware.ino
@@ -5,13 +5,16 @@
 #define rt_sw_pin 5
 #define disp_clk_pin 6
 #define disp_dio_pin 7
-#define interrupt_pin 2
+#define play_pause_pin 2
+#define stop_pin 10
 
 int rotation;
 int value;
 bool skip = false;
 bool active = false;
+bool stop_flag = false;
 
+int initial_seconds = 0;
 int seconds = 0;
 int new_seconds = 0;
 
@@ -26,25 +29,31 @@ void setup() {
   pinMode (rt_clk_pin,INPUT);
   pinMode (rt_dt_pin,INPUT);
   pinMode (rt_sw_pin,INPUT);
-  pinMode (interrupt_pin,INPUT);
+  pinMode (play_pause_pin,INPUT);
+  pinMode (stop_pin,INPUT);
   rotation = digitalRead(rt_clk_pin);
+  Serial.println("Start");
 
   disp.clear();
   disp.brightness(7);  // яркость, 0 - 7 (минимум - максимум)
   displayTime(seconds);
 
-  attachInterrupt(digitalPinToInterrupt(interrupt_pin), process_interrupt_debounced, HIGH);
+  attachInterrupt(digitalPinToInterrupt(play_pause_pin), process_play_pause, RISING);
+  attachInterrupt(digitalPinToInterrupt(stop_pin), process_stop, RISING);
 }
 
 void loop() {
 
+  if (stop_flag){
+    stop();
+    stop_flag = false;
+  }
+
   if (active){
     delay(1000);
-    if(active) { //Check for the case interrupt has been triggered
-      new_seconds--;
-      if(new_seconds == 0){
-        deactivate();
-      }
+    new_seconds--;
+    if(new_seconds == 0){
+      deactivate();
     } 
   }  
   else {    
@@ -73,6 +82,7 @@ void loop() {
 
   if (seconds != new_seconds){
     seconds = new_seconds;
+    initial_seconds = new_seconds;
     displayTime(seconds);
   }
 }
@@ -103,8 +113,15 @@ void activate(){
   active = true;
   digitalWrite(output_pin, 1);
 }
+
 void deactivate(){
   active = false;
+  digitalWrite(output_pin, 0);
+}
+
+void stop(){
+  active = false;
+  new_seconds = initial_seconds;
   digitalWrite(output_pin, 0);
 }
 
@@ -113,9 +130,17 @@ void switch_state(){
   else if (active) deactivate();
 }
 
-void process_interrupt_debounced(){
+void process_play_pause(){
   if(millis() - lastDebounceTime > debounceDelay){
     switch_state();
+    lastDebounceTime = millis();    
+  }  
+}
+
+void process_stop(){  
+  Serial.println("Stop interrupt");
+  if(millis() - lastDebounceTime > debounceDelay){
+    stop_flag = true;
     lastDebounceTime = millis();    
   }  
 }
